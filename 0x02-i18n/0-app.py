138a6bbf4b0f93aa5d7ab88919f7ae6ef928d333
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2023-08-02 00:48:30 +0100

    Add first task and README

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic Flask App."""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Handle route."""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0', debug=True)
